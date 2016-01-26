da90dfe8d8a2776d0ff4ddcf91487490143aa878
Michael Cavalea <callmecavs@gmail.com>
2016-01-25 19:29:18 -0500

    add working method name helper

--- src/maximize.js
+++ src/maximize.js
@@ -1,3 +1,16 @@
-export default () => {
+// determine method name, using the documentElement
 
+function getName() {
+  const names = [
+    'requestFullscreen',
+    'msRequestFullscreen',
+    'mozRequestFullScreen',
+    'webkitRequestFullscreen'
+  ]
+
+  return names.reduce((prev, curr) => document.documentElement[curr] ? curr : prev, undefined)
+}
+
+export default () => {
+  const method = getName()
 }
