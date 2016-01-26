23a00e4dae5d97e614382eb9be7b9c0269f630dd
Michael Cavalea <callmecavs@gmail.com>
2016-01-25 21:30:14 -0500

    improve commenting

--- src/maximize.js
+++ src/maximize.js
@@ -6,18 +6,18 @@ function getName() {
     'webkitRequestFullscreen'
   ]
 
-  // determine method name, using the documentElement
   return names.reduce((prev, curr) => document.documentElement[curr] ? curr : prev, undefined)
 }
 
 export default (toggle, target = document.documentElement) => {
+  // determine fullscreen method
   const method = getName()
 
   toggle.addEventListener('click', event => {
-    // prevent default if toggle is a link
+    // prevent default if toggle is link
     toggle.tagName.toLowerCase() === 'a' && event.preventDefault()
 
-    // enter fullscreen
-    target[method]()
+    // enter fullscreen, or fail silently
+    target[method] && target[method]()
   })
 }
